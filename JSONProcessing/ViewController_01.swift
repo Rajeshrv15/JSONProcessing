d25841723350a62114d23773db1d84775e892794
Software AG <rajeshkumarrv@hotmail.com>
2018-11-05 16:48:17 +0530

    Changes to support Rest POST call

--- JSONProcessing/ViewController_01.swift
+++ JSONProcessing/ViewController_01.swift
@@ -14,11 +14,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        makePostCall(strURL: "https://alfabet.agileappscloud.eu/networking/rest/login", strContent: "<platform><login><userName>userNikarin</userName><password>Theriyin18</password></login></platform>")
-        print("Going to call second time")
-        makePostCall(strURL: "https://alfabet.agileappscloud.eu/networking/rest/record/cases", strContent: "<platform><record><subject>PeriodicMaintenance</subject><description>Maintenance for the device is created.</description><type>1</type><priority>4</priority><account>1753289431</account><status>1</status><from_address>bath@softwareag.com</from_address></record></platform>")
-        return
-        
         let sZemantisURL : String = "http://10.60.5.238:9083/adapars/apply/drill_pmml?record={\"RPM\": 2350,\"Temperature\": 52,\"Sound\": 3.2}"
         let strZemantisResDict = GetDeviceMetricsFromServer(anAccessURL: sZemantisURL, anUserName: "Administrator", anPassword: "manage", bSync: true)
         print("Response received \(strZemantisResDict)")
@@ -27,75 +22,9 @@ class ViewController: UIViewController {
         print(sUIVal)
     }
     
-    func makePostCall(strURL: String, strContent: String) {
-        let anSem = DispatchSemaphore.init(value: 0)
-        //let todosEndpoint: String = "https://alfabet.agileappscloud.eu/networking/rest/login"
-        //let todosEndpoint: String = "https://alfabet.agileappscloud.eu/networking/rest/record/cases"
-        let todosEndpoint: String  = strURL
-        guard let todosURL = URL(string: todosEndpoint) else {
-            print("Error: cannot create URL")
-            return
-        }
-        var todosUrlRequest = URLRequest(url: todosURL)
-        todosUrlRequest.httpMethod = "POST"
-        //let newTodo: String = "<platform><login><userName>userNikarin</userName><password>Theriyin18</password></login></platform>"
-        //let newTodo: String = "<platform><record><subject>PeriodicMaintenance</subject><description>Maintenance for the device is created.</description><type>1</type><priority>4</priority><account> </account><status>1</status><from_address>bath@softwareag.com</from_address></record></platform>"
-        let newTodo: String = strContent
-        let jsonTodo: Data
-        do {
-            /*jsonTodo = try JSONSerialization.data(withJSONObject: newTodo, options: [])
-            todosUrlRequest.httpBody = jsonTodo*/
-            
-            todosUrlRequest.addValue("application/xml", forHTTPHeaderField: "Content-Type")
-            todosUrlRequest.httpBody = newTodo.data(using: String.Encoding.utf8)
-        } catch {
-            print("Error: cannot create JSON from todo")
-            return
-        }
-        
-        let session = URLSession.shared
-        
-        let task = session.dataTask(with: todosUrlRequest) {
-            (data, response, error) in
-            guard error == nil else {
-                print("error calling POST on /todos/1")
-                print(error!)
-                return
-            }
-            guard let responseData = data else {
-                print("Error: did not receive data")
-                return
-            }
-            print("Anj test response \(responseData)")
-            // parse the result as JSON, since that's what the API provides
-            do {
-                guard let receivedTodo = try JSONSerialization.jsonObject(with: responseData,
-                                                                          options: []) as? [String: Any] else {
-                                                                            print("Could not get JSON from responseData as dictionary")
-                                                                            return
-                }
-                print("The todo is: " + receivedTodo.description)
-                
-                /*guard let todoID = receivedTodo["description"] as? Int else {
-                    print("Could not get todoID as int from JSON")
-                    return
-                }
-                print("The sessionId is: \(todoID)")*/
-            } catch  {
-                print("error parsing response from POST on /todos")
-                return
-            }
-            anSem.signal()
-        }
-        task.resume()
-        anSem.wait(timeout: .distantFuture)
-    }
-    
-    
     //Read device metrics from Server URL
     func GetDeviceMetricsFromServer(anAccessURL : String, anUserName: String, anPassword: String, bSync: Bool) -> String {
         let config = URLSessionConfiguration.default
-        
         var strResponse : String = ""
         let anSem = DispatchSemaphore.init(value: 0)
         
